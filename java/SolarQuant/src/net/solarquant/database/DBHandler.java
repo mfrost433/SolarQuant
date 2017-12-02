a07c696fd85be14b86c18c897d6aa7d3f327765a
mfrost433 <mfrost433@gmail.com>
2017-12-02 20:30:52 +1300

    fixed .gitignorey

---
+++ java/SolarQuant/src/net/solarquant/database/DBHandler.java
@@ -0,0 +1,94 @@
+package net.solarquant.database;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import net.solarquant.util.StatusEnum;
+
+
+public class DBHandler {
+
+	private static final String CONNECTION_STRING = "jdbc:mysql://localhost/solarquant?" +
+			"user=solarquant&password=solarquant";
+
+	private Connection conn_;
+	public DBHandler() {
+		getConnection();
+	}
+
+	private void getConnection() {
+		try {
+			conn_ =	DriverManager.getConnection(CONNECTION_STRING);
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+	}
+
+	public Request getOldestRequest(String tableName, StatusEnum status){
+		Statement stmt = null;
+		try {
+			String query = "SELECT * FROM %s WHERE STATUS = %s "
+					+ "ORDER BY DATE_REQUESTED ASC LIMIT 1";
+			query = String.format(query, tableName, status.getStateId(), tableName);
+			stmt = conn_.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			if (rs.next() == false) {
+				return null;
+			}else {
+				return new Request(rs.getDate("DATE_REQUESTED"), rs.getString("REQUEST_ENGINE")
+						, rs.getInt("STATUS"), rs.getInt("REQUEST_ID"), rs.getInt("NODE_ID"), rs.getString("SOURCE_ID"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public void updateRequestStatus(String tableName, int reqId, int newStatus) {
+		Statement stmt = null;
+		try {
+			String query = "UPDATE %s SET STATUS = "+newStatus+" WHERE REQUEST_ID = %s";
+			query = String.format(query, tableName, reqId);
+			stmt = conn_.createStatement();
+			stmt.execute(query);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Date getLatestTrainingDataDate(Request r) {
+		String query = "SELECT ENTRY_DATE FROM %s WHERE NODE_ID = %s AND SOURCE_ID = '%s' "
+				+ "ORDER BY ENTRY_DATE DESC LIMIT 1";
+		
+		if(r.getEngineName().equalsIgnoreCase("tensorflow")) {
+			query = String.format(query, "tensorflow_training_input", r.getNodeId(), r.getSourceId());
+		}
+		//System.out.println(query);
+		
+		Statement stmt;
+		try {
+			stmt = conn_.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			if(rs.next() == true) {
+				Date out = rs.getDate("ENTRY_DATE");
+				return out;
+			}else {
+				return null;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+}
---
+++ java/SolarQuant/src/net/solarquant/database/Request.java
@@ -0,0 +1,63 @@
+package net.solarquant.database;
+
+import java.sql.Date;
+
+import net.solarquant.util.StatusEnum;
+
+
+/**
+ * This class represents a user request for training or predictions, and encapsulates the data, and allows
+ * for status updates
+ * @author matthew
+ *
+ */
+public class Request {
+	private DBHandler d = new DBHandler();
+	private Date date_;
+	private String engine_;
+	private StatusEnum status_;
+	private int requestId_;
+	private int nodeId_;
+	private String sourceId_;
+
+	public Request(Date date, String engine, int status, int requestId, int nodeId, String sourceId) {
+
+		date_ = date;
+		engine_ = engine;
+		status_ = StatusEnum.fromInt(status);		
+		requestId_ = requestId;
+		nodeId_ = nodeId;
+		sourceId_ = sourceId;
+
+	}
+
+	public Date getRequestDate() {
+		return date_;
+	}
+
+	public String getEngineName() {
+		return engine_;
+	}
+
+	public StatusEnum getStatus() {
+		return status_;
+	}
+	
+	public int getRequestId() {
+		return requestId_;
+	}
+	public int getNodeId() {
+		return nodeId_;
+	}
+	
+	public String getSourceId() {
+		return sourceId_;
+	}
+	
+	public void updateStatus(StatusEnum newStatus) {
+		
+		d.updateRequestStatus("training_requests", requestId_, newStatus.getStateId());		
+		
+	}
+
+}
